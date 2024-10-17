8e1034cdcef248d40b49ae4a2d2a36836aae9fd9
202402060 <166345626+kg112345@users.noreply.github.com>
2024-10-17 15:47:31 +0900

    Update day08-1.c

--- day08/day08-1.c
+++ day08/day08-1.c
@@ -3,22 +3,22 @@
 
 int isAlphabet(char *str) {
     for (int i = 0; i < strlen(str)-1; i++) { // 문자열타입은 마지막에 공백이 오기땜에 -1 해줌
-        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
-            return 1;
+        if (!((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))) {
+            return 0;
         }
     }
-    return 0;
+    return 1;
 }
 
 int main(void) {
     char string[100]; 
 
     printf("입력: ");
-    scanf_s("%s", string, sizeof(string));
+    fgets(string, sizeof(string), stdin);
+ 
 
     int n = strlen(string); //문자열 길이
-
-    printf("%d", n);
+    
 
     if (isAlphabet(string)) {
         for (int i = 0; i < n / 2 ; i++) {  // 대칭으로 교환하면 되므로 n/2번만 하면 됨
