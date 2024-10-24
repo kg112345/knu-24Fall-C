0d50e3c35ac2c74b0a3ede0212d010a2e485dad6
202402060 <166345626+kg112345@users.noreply.github.com>
2024-10-24 13:09:59 +0900

    Update day09-2.c

--- day09/day09-2.c
+++ day09/day09-2.c
@@ -14,30 +14,29 @@ void printProduct(struct Product product) {
 
 int main(void) {
 	struct Product productArray[5];
-	int count = 0, choice;
+	int hoice;
+	int i;
 
-	while (count < 5) {
+	for (i = 0; i < 5; i++) {
 		printf("상품 정보 입력(입력 중단은 id에 0 입력)\n");
 
 		printf("상품 id:");
-		scanf_s("%d", &productArray[count].id);
-		if (productArray[count].id == 0) {
+		scanf_s("%d", &productArray[i].id);
+		if (productArray[i].id == 0) {
 			break;
 		}
 
 		printf("상품 name:");
-		scanf_s("%s", &productArray[count].name, sizeof(productArray[count].name));
-		
+		scanf_s("%s", &productArray[i].name, sizeof(productArray[i].name));
+
 		printf("상품 price:");
-		scanf_s("%d", &productArray[count].price);
+		scanf_s("%d", &productArray[i].price);
 
 		printf("\n");
-
-		count++;
 	}
-	
+
 	printf("\n\n<<입력한 상품>>\n");
-	for (int i = 0; i < count; i++) {
-		printProduct(productArray[i]);
+	for (int j = 0; j < i; j++) {
+		printProduct(productArray[j]);
 	}
 }
